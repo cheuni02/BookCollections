e98a52ba00e10767145ed1e2346a1e72898d7ce1
cheuni02 <inhcheung@gmail.com>
2025-10-30 12:24:35 +0000

    wrapped seeder into a class

--- seed_database.py
+++ seed_database.py
@@ -1,49 +1,52 @@
 from api import app, db, BookModel
 
-# just for testing purposes
-seeded_records = [
-  {
-    "title": "A Particularly Nasty Case",
-    "author": "Adam Kay",
-    "year_published": 2025,
-    "genre": "Fiction"
-  },
-  {
-    "title": "Night Swimmers",
-    "author": "Roisin Maguire",
-    "year_published": 2024,
-    "genre": "Fiction"
-  },
-  {
-    "title": "Green Dot",
-    "author": "Madeleine Gray",
-    "year_published": 2024,
-    "genre": "Humour"
-  },
-  {
-    "title": "The Cyclist",
-    "author": "Tim Sullivan",
-    "year_published": 2021,
-    "genre": "Crime fiction thriller"
-  },
-  {
-    "title": "Onyx Storm (The Empyrean, #3)",
-    "author": "Rebecca Yarros",
-    "year_published": 2025,
-    "genre": "Fantasy"
-  }
-]
+class BookDBSeeder:
+    seeded_records = [
+      {
+        "title": "A Particularly Nasty Case",
+        "author": "Adam Kay",
+        "year_published": 2025,
+        "genre": "Fiction"
+      },
+      {
+        "title": "Night Swimmers",
+        "author": "Roisin Maguire",
+        "year_published": 2024,
+        "genre": "Fiction"
+      },
+      {
+        "title": "Green Dot",
+        "author": "Madeleine Gray",
+        "year_published": 2024,
+        "genre": "Humour"
+      },
+      {
+        "title": "The Cyclist",
+        "author": "Tim Sullivan",
+        "year_published": 2021,
+        "genre": "Crime fiction thriller"
+      },
+      {
+        "title": "Onyx Storm (The Empyrean, #3)",
+        "author": "Rebecca Yarros",
+        "year_published": 2025,
+        "genre": "Fantasy"
+      }
+    ]
+
+    def read_seed_db_from_json(self):
+        with app.app_context():
+            for record in self.seeded_records:
+                print(record["title"])
+                book = BookModel(
+                    title=record["title"],
+                    author=record["author"],
+                    year_published=record["year_published"],
+                    genre=record["genre"],
+                )
+                db.session.add(book)
+            db.session.commit()
 
 
 if __name__ == "__main__":
-    with app.app_context():
-        for record in seeded_records:
-            print(record["title"])
-            book = BookModel(
-                title=record["title"],
-                author=record["author"],
-                year_published=record["year_published"],
-                genre=record["genre"],
-            )
-            db.session.add(book)
-        db.session.commit()
+    BookDBSeeder().read_seed_db_from_json()
