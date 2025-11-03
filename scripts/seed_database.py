e57e7145911109216fc5713a0dad18aaf1e4dd4b
cheuni02 <inhcheung@gmail.com>
2025-11-03 23:41:31 +0000

    make seeding script read from json file instead of reading json inside it

--- scripts/seed_database.py
+++ scripts/seed_database.py
@@ -1,3 +1,4 @@
+import json
 import sys
 import time
 from importlib import resources
@@ -9,51 +10,21 @@ from app import BookModel, app, db
 
 
 class BookDBSeeder:
-    seeded_records = [
-        {
-            "title": "A Particularly Nasty Case",
-            "author": "Adam Kay",
-            "year_published": 2025,
-            "genre": "Fiction",
-        },
-        {
-            "title": "Night Swimmers",
-            "author": "Roisin Maguire",
-            "year_published": 2024,
-            "genre": "Fiction",
-        },
-        {
-            "title": "Green Dot",
-            "author": "Madeleine Gray",
-            "year_published": 2024,
-            "genre": "Humour",
-        },
-        {
-            "title": "The Cyclist",
-            "author": "Tim Sullivan",
-            "year_published": 2021,
-            "genre": "Crime fiction thriller",
-        },
-        {
-            "title": "Onyx Storm (The Empyrean, #3)",
-            "author": "Rebecca Yarros",
-            "year_published": 2025,
-            "genre": "Fantasy",
-        },
-    ]
 
-    def seed_db_from_json(self):
+    def seed_db_from_json(self, filename):
         with app.app_context():
-            for record in self.seeded_records:
-                print(record["title"])
-                book = BookModel(
-                    title=record["title"],
-                    author=record["author"],
-                    year_published=record["year_published"],
-                    genre=record["genre"],
-                )
-                db.session.add(book)
-            db.session.commit()
+            with resources.files("data").joinpath(filename).open() as f:
+                books = json.load(f)
+                for record in books:
+                    print(record["title"])
+                    book = BookModel(
+                        title=record["title"],
+                        author=record["author"],
+                        year_published=record["year_published"],
+                        genre=record["genre"],
+                    )
+                    db.session.add(book)
+                db.session.commit()
 
     def seed_db_from_csv(self, filename):
         with app.app_context():
@@ -75,25 +46,27 @@ class BookDBSeeder:
 if __name__ == "__main__":
     print("Seeding database with some book data ...")
     time.sleep(1)
-    filename = "seeded_book_records.csv"
+    csv_filename = "seeded_book_records.csv"
+    json_filename = "seeded_book_records.json"
     while True:
         choice = int(
             input(
                 "Choose from the following:\n"
                 "1. Seed from JSON file\n"
                 "2. Seed from CSV file\n"
+                "Please enter your choice:\n"
             )
         )
         try:
             match (choice):
                 case 1:
                     print("you chose to seed with JSON file\n")
-                    BookDBSeeder().seed_db_from_json()
+                    BookDBSeeder().seed_db_from_json(json_filename)
                     break
                 case 2:
                     print("you chose to seed with CSV file\n")
-                    print(f"will read from this file: {filename}\n")
-                    BookDBSeeder().seed_db_from_csv(filename)
+                    print(f"will read from this file: {csv_filename}\n")
+                    BookDBSeeder().seed_db_from_csv(csv_filename)
                     break
                 case _:
                     print("you must choose either 1 or 2, try again ...\n")
@@ -102,5 +75,9 @@ if __name__ == "__main__":
                 f"Error in seeding - has the database already been seeded?\n"
                 f"See stacktrace below:\n{e}"
             )
-        except Exception:
+        except Exception as e:
             print("Unexpected error:", sys.exc_info()[0])
+            print(f"See stacktrace below:\n{e}")
+        finally:
+            print("taking you back to the options screen\n")
+            time.sleep(2)
